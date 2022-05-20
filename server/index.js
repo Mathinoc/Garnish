512b78eb138d13e3ba4c8cd58760202cb1190aa3
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-05-20 20:20:18 +0200

    Editing READ.me ; removed unused files and code

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const router = require('./router');
 const cors = require('cors');
 require('dotenv').config();
 const PORT = process.env.SERVER_PORT || 3001;
-// const mongoose = require('./models/index_model.js');
 
 const app = Express();
 
@@ -12,6 +11,4 @@ app.use(cors());
 app.use(Express.json());    //parse the req into json
 app.use(router);
 
-// mongoose.connection.on('connected', ()=>{
-  app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT} 🪁`));
-// })
+app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT} 🪁`));
