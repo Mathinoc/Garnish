470298761b3bfff748f7f8201888dad9a62c2e7f
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-05-27 13:14:01 +0200

    heroku commit

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 const Express = require('express');
 const router = require('./router');
+const path = require('path')
 const cors = require('cors');
+const { config } = require('dotenv');
 require('dotenv').config();
 const PORT = process.env.PORT || process.env.SERVER_PORT || 3001;
 
 const app = Express();
 
+config();
+
 app.use(cors());
 
 app.use(Express.json());    //parse the req into json
+
+app.use(Express.static(path.join(__dirname + './../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './../client/build/index.html'));
+});
+
 app.use(router);
 
 app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT} 🪁`));
